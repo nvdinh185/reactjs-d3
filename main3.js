5efef55a48c31bc2bc1f4b8aefc9975eeec3ddc2
nvdinh185 <nvdinh0766777123@gmail.com>
2023-09-23 08:45:34 +0700

    x

--- main3.js
+++ main3.js
@@ -52,7 +52,6 @@ class AppComponent extends React.Component {
   }
 }
 
-// ReactDOM.render(<AppComponent />, document.getElementById("root"))
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
--- main4.js
+++ main4.js
@@ -79,7 +79,6 @@ class AppComponent extends React.Component {
   }
 }
 
-// ReactDOM.render(<AppComponent listNews={listNews} />, document.getElementById("root"))
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
